1832e7f1c6ce9522644326db32676f77d4d11b49
Akira Masuda <masuda.akira1014@gmail.com>
2016-06-05 13:14:02 +0900

    Completed making landing page.

--- app.js
+++ app.js
@@ -4,11 +4,9 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
-var routes = require('./routes/index');
-var users = require('./routes/users');
-
 var app = express();
+
+var port_num = 13337;
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -16,14 +14,19 @@ app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+// use is for using middlware
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
+var routes = require('./routes');
+app.use('/', routes.index);
+//app.use('/users', users);
+
+
+// error handlers======================
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -31,11 +34,7 @@ app.use(function(req, res, next) {
   err.status = 404;
   next(err);
 });
-
-// error handlers
-
-// development error handler
-// will print stacktrace
+// development error handler will print stacktrace
 if (app.get('env') === 'development') {
   app.use(function(err, req, res, next) {
     res.status(err.status || 500);
@@ -45,9 +44,7 @@ if (app.get('env') === 'development') {
     });
   });
 }
-
-// production error handler
-// no stacktraces leaked to user
+// production error handler. no stacktraces leaked to user
 app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error', {
@@ -56,5 +53,45 @@ app.use(function(err, req, res, next) {
   });
 });
 
+//=====================================
+
+var session = require('express-session');
+// setting of mongo db
+var MongoStore = require('connect-mongo')(session);
+var mongoose = require('mongoose');
+
+app.use(session({
+    secret: 'secret',
+    store: new MongoStore({
+        mongooseConnection: mongoose.connection,
+		db: 'session',
+        host: 'localhost',
+        clear_interval: 60 * 60,
+    }),
+    cookie: {
+        httpOnly: false,
+        maxAge: new Date(Date.now() + 60 * 60 * 1000)
+    },
+	resave: true,
+	saveUninitialized: true 
+})); //追加
+
+var loginCheck = function(req, res, next) {
+    if(req.session.user){
+      next();
+    }else{
+      res.redirect('/login');
+    }
+};
+
+//app.get('/', loginCheck, routes.index);
+//app.get('/login', routes.login);
+//app.post('/add', routes.add);
+//app.get('/logout', function(req, res){
+//  req.session.destroy();
+//  console.log('deleted sesstion');
+//  res.redirect('/');
+//});
 
 module.exports = app;
+app.listen(port_num);
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,56 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+// a middleware function with no mount path. This code is executed for every request to the router
+//router.use(function (req, res, next) {
+//  console.log('Time:', Date.now());
+//    next();
+//	});
+//}
+
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index.ejs', { title: 'BANANAS' }); // pass dictionary obj to 'index.ejs'
 });
 
 module.exports = router;
+
+/*モデル読み込み*/
+var model = require('../model.js'),
+    User  = model.User;
+
+/*ログイン後ページ*/
+exports.index = function(req, res){
+    res.render('index', { user: req.session.user});
+    console.log(req.session.user);
+};
+
+/*ユーザー登録機能*/
+exports.add = function(req, res){
+    var newUser = new User(req.body);
+    newUser.save(function(err){
+        if(err){
+            console.log(err);
+            res.redirect('back');
+        }else{
+            res.redirect('/');
+        }
+    });
+};
+
+/*ログイン機能*/
+exports.login = function(req, res){
+    var email    = req.query.email;
+    var password = req.query.password;
+    var query = { "email": email, "password": password };
+    User.find(query, function(err, data){
+        if(err){
+            console.log(err);
+        }
+        if(data == ""){
+            res.render('login');
+        }else{
+            req.session.user = email;
+            res.redirect('/');
+        }
+    });
+};
